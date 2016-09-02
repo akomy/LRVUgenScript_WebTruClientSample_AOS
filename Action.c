344c16b1b49fcb08e6d2d46d78dbc7979cde462e
unknown <Administrator@WIN-85G9V2VDAAB>
2016-09-02 17:20:45 +0900

    スクリプト記録し直し

--- Action.c
+++ Action.c
@@ -4,15 +4,21 @@
 
 Action()
 {
-	truclient_step("3", "'http://www.advantageonlineshopping.com' に 移動", "snapshot=Action_3.inf");
-	truclient_step("4", "LAPTOPS Shop Now を クリック", "snapshot=Action_4.inf");
-	truclient_step("6", "image (2) 画像 を クリック", "snapshot=Action_6.inf");
-	truclient_step("7", "GRAY を クリック", "snapshot=Action_7.inf");
-	truclient_step("8", "Plus を クリック", "snapshot=Action_8.inf");
-	truclient_step("9", "ADD TO CART ボタン を クリック", "snapshot=Action_9.inf");
-	truclient_step("11", "element (3) を クリック", "snapshot=Action_11.inf");
-	truclient_step("13", "element (2) を クリック", "snapshot=Action_13.inf");
-	truclient_step("15", "HOME リンク を クリック", "snapshot=Action_15.inf");
+	lr_start_transaction("Top Screen");
+	truclient_step("1", "'http://www.advantageonlineshopping.com' に 移動", "snapshot=Action_1.inf");
+	truclient_step("2", "LAPTOPS Shop Now を クリック", "snapshot=Action_2.inf");
+	lr_end_transaction("Top Screen",0);
+	lr_start_transaction("Select Laptop");
+	truclient_step("4", "image (2) 画像 を クリック", "snapshot=Action_4.inf");
+	truclient_step("5", "RED を クリック", "snapshot=Action_5.inf");
+	truclient_step("6", "Plus を クリック", "snapshot=Action_6.inf");
+	truclient_step("8", "ADD TO CART ボタン を クリック", "snapshot=Action_8.inf");
+	lr_end_transaction("Select Laptop",0);
+	lr_start_transaction("Cart Clear");
+	truclient_step("10", "element (4) を クリック", "snapshot=Action_10.inf");
+	truclient_step("12", "REMOVE を クリック", "snapshot=Action_12.inf");
+	truclient_step("13", "CONTINUE SHOPPING リンク を クリック", "snapshot=Action_13.inf");
+	lr_end_transaction("Cart Clear",0);
 
 	return 0;
 }
