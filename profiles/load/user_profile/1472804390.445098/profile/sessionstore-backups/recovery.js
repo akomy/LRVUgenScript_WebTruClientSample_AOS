2150fc93415aafe1b3f24c105ee38c3bbaec49c3
unknown <Administrator@WIN-85G9V2VDAAB>
2016-09-02 23:21:24 +0900

    テキストチェックを挿入

--- profiles/load/user_profile/1472804390.445098/profile/sessionstore-backups/recovery.js
+++
@@ -1,1 +0,0 @@
-{"windows":[{"tabs":[],"selected":0,"_closedTabs":[],"busy":false,"width":1311,"height":888,"screenX":144,"screenY":6,"sizemode":"normal"}],"selectedWindow":1,"_closedWindows":[],"session":{"lastUpdate":1472804410024,"startTime":1472804391056,"recentCrashes":0},"global":{}}
---
+++ profiles/load/user_profile/1472826019.643824/profile/sessionstore-backups/recovery.js
@@ -0,0 +1,1 @@
+{"windows":[{"tabs":[],"selected":0,"_closedTabs":[],"busy":false,"width":1311,"height":888,"screenX":144,"screenY":6,"sizemode":"normal"}],"selectedWindow":1,"_closedWindows":[],"session":{"lastUpdate":1472826039054,"startTime":1472826020301,"recentCrashes":0},"global":{}}
