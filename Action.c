913efb29eb39f8c6b1230a22a1d287f3503968a2
unknown <Administrator@WIN-85G9V2VDAAB>
2016-09-02 16:21:54 +0900

    再生時にカート表示をする操作のオブジェクトが複数認識されたため、修正を実施

--- Action.c
+++ Action.c
@@ -4,14 +4,15 @@
 
 Action()
 {
-	truclient_step("1", "'http://www.advantageonlineshopping.com/' に 移動", "snapshot=Action_1.inf");
-	truclient_step("2", "LAPTOPS Shop Now を クリック", "snapshot=Action_2.inf");
-	truclient_step("4", "image (1) 画像 を クリック", "snapshot=Action_4.inf");
-	truclient_step("5", "RED を クリック", "snapshot=Action_5.inf");
-	truclient_step("6", "Plus を クリック", "snapshot=Action_6.inf");
-	truclient_step("7", "ADD TO CART ボタン を クリック", "snapshot=Action_7.inf");
-	truclient_step("9", "element (2) を クリック", "snapshot=Action_9.inf");
-	truclient_step("11", "HOME リンク を クリック", "snapshot=Action_11.inf");
+	truclient_step("3", "'http://www.advantageonlineshopping.com' に 移動", "snapshot=Action_3.inf");
+	truclient_step("4", "LAPTOPS Shop Now を クリック", "snapshot=Action_4.inf");
+	truclient_step("6", "image (2) 画像 を クリック", "snapshot=Action_6.inf");
+	truclient_step("7", "GRAY を クリック", "snapshot=Action_7.inf");
+	truclient_step("8", "Plus を クリック", "snapshot=Action_8.inf");
+	truclient_step("9", "ADD TO CART ボタン を クリック", "snapshot=Action_9.inf");
+	truclient_step("11", "element (3) を クリック", "snapshot=Action_11.inf");
+	truclient_step("13", "element (2) を クリック", "snapshot=Action_13.inf");
+	truclient_step("15", "HOME リンク を クリック", "snapshot=Action_15.inf");
 
 	return 0;
 }
